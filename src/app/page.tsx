5b9a19b41cb4f0d765a604e09cdee924f0b351f2
Nutthawut Srinamol <oat@odds.team>
2025-08-03 13:58:56 +0700

    add project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import Image from "next/image";
-
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "@/styles/components/ui/avatar";
-import { Card, CardContent, CardFooter } from "@/styles/components/ui/card";
 import RecipeCard from "@/components/recipe-card";
 
 const Home = () => {
